70920900c6d14f107feaf4244e7eee45e6101e6f
clm21020 <clm21020@gmail.com>
2015-03-26 13:13:46 -0700

    initial commit

---
+++ Tabs/tabs.js
@@ -0,0 +1,27 @@
+$.Tabs = function (el) { //el = ul
+  this.$el = $(el);
+  var contentTabsId = this.$el.data("content-tabs");
+  this.$contentTabs = $(contentTabsId);
+  this.$activeTab = this.$contentTabs.find(".active");
+  this.bindEvents();
+};
+
+$.fn.tabs = function () {
+  return this.each(function () {
+    new $.Tabs(this);
+  });
+};
+
+$.Tabs.prototype.bindEvents = function(){
+  this.$el.on("click", "a", this.clickTab.bind(this));
+};
+
+$.Tabs.prototype.clickTab = function(event){
+  event.preventDefault;
+  this.$el.find('a').removeClass("active");
+  this.$activeTab.removeClass("active");
+  var $clickedLink = $(event.currentTarget);
+  $clickedLink.addClass('active');
+  var contentId = $clickedLink.attr('href');
+  this.$activeTab = this.$contentTabs.find(contentId).addClass('active');
+};
